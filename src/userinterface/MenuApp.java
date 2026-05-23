ec563ece1ef5055735ebb8b27fe2bb74744ba521
Brabian-Perez <perezvelezbrahiandavid@gmail.com>
2026-05-23 10:17:33 -0500

    se crea validaciones al momento de  guardar usuarios en la base de datos

--- src/userinterface/MenuApp.java
+++ src/userinterface/MenuApp.java
@@ -76,6 +76,7 @@ public class MenuApp {
         );
 
         clienteService.registrar(cliente);
+        System.out.println("Cliente registrado exitosamente");
     }
 
     // =========================
